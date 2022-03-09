9d9edfe3487c667bbc058aa8c0a0db48cb4e8c3f
JonkHunkle <88416486+JonkHunkle@users.noreply.github.com>
2022-03-08 17:50:34 -0800

    Updates

--- src/components/Resume.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function Resume() {
-    return (
-        <div className='flex h-screen w-full flex-col bg-gradient-to-l from-indigo-200  via-rose-600 via-purple-300 to-pink-300 '>
-            <div>Resume</div></div>
-    )
-}
